04d2d136cdf6daeaeb49116247379301211536f0
Eleazar <programador3693@gmail.com>
2025-10-02 20:19:22 -0600

    Agregue logica de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { MateriaLista } from './materia-lista/materia-lista';
 
 //http:localhost:4200/materias
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'materias', component: MateriaLista},
+    {path: '', redirectTo: 'materias', pathMatch: 'full'}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 //import { RouterOutlet } from '@angular/router';
-import { MateriaLista } from './materia-lista/materia-lista';
+//import { MateriaLista } from './materia-lista/materia-lista';
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet],
   templateUrl: './app.html',
   //styleUrl: './app.css',
-  imports: [MateriaLista]
+  imports: [RouterModule]
 })
 export class App {
   protected readonly title = signal('menu-app');
